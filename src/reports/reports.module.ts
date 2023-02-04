54ac95c45745939760b5b3ad78f857837a9719cc
i-zokirov <izokirov@outlook.com>
2023-02-04 19:07:52 +0100

    typeorm and sqlite hookup

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { ReportsService } from './reports.service';
-import { ReportsController } from './reports.controller';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ReportsService } from "./reports.service";
+import { ReportsController } from "./reports.controller";
+import Report from "./report.entity";
 
 @Module({
-  providers: [ReportsService],
-  controllers: [ReportsController]
+    imports: [TypeOrmModule.forFeature([Report])],
+    providers: [ReportsService],
+    controllers: [ReportsController],
 })
 export class ReportsModule {}
